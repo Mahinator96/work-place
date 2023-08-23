ec7c6f91a9482997c3be1a847c68d05ce271613c
Evgeniy Vasiliev <vasilievdevelop@gmail.com>
2023-08-23 11:58:48 +0300

    Load with API location in select

--- js/script.js
+++ js/script.js
@@ -1,6 +1,62 @@
-const citySelect = document.querySelector('#city');
+/* 
+	Работа с сервером
+*/
+const API_URL = "https://workspace-methed.vercel.app/";
+const LOCATION_URL = "api/locations";
 
-const cityChoices = new Choices(citySelect, {
-	searchEnabled: false,
-	itemSelectText: '',
-});
+/* 
+	Ф-ия для получения данных
+*/
+const getData = async (url, cbSuccess, cbError) => {
+	try {
+		const response = await fetch(url);
+		const data = await response.json();
+		
+		cbSuccess(data);
+	}
+	catch(err) {
+		console.log(err);
+	}
+}
+
+/* 
+	Инициализация при запуске приложения
+*/
+const init = () => {
+	/* 
+		Работа с choices.js
+	*/
+	const citySelect = document.querySelector('#city');
+
+	const cityChoices = new Choices(citySelect, {
+		// searchEnabled: false,
+		itemSelectText: '',
+	});
+
+	/* 
+		Получение городов
+	*/
+	getData(
+		`${API_URL}${LOCATION_URL}`,
+		
+		// Перебор массива для проебразования в нужный формат для choices.setChoices
+		(locationData) => {
+			const locations = locationData.map((location) => ({value: location,}));
+
+			// Добавляем в choices данные которые приходят с сервера
+			cityChoices.setChoices(
+				locations,
+				'value',
+				'label',
+				true
+			)
+		}, 
+
+		(err) => console.log(err)
+	);
+	
+}
+
+init();
+
+
